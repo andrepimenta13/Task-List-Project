1c408300ce8fac50b2d8efac0d6271a5f0c9aa26
Andre Pimenta <trz.andre@gmail.com>
2022-02-01 21:26:51 +0000

    new funcs

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const inputElement = document.querySelector('.new-task-input');
 const addTaskButton = document.querySelector('.new-task-button');
 
+const tasksContainer = document.querySelector('.tasks-container');
+
 const validateInput = () => {
     if (inputElement.value.trim().length > 0) {
         return true
@@ -15,6 +17,24 @@ const handleAddTask = () => {
     if (!inputIsValid) {
         return inputElement.classList.add('error');
     }
+
+    const taskItemContainer = document.createElement('div')
+    taskItemContainer.classList.add('task-item')
+
+    const taskContent = document.createElement('p')
+    taskContent.innerText = inputElement.value;
+
+    const deleteItem = document.createElement('i');
+    deleteItem.classList.add('far');
+    deleteItem.classList.add('fa-trash-alt');
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = '';
+
 };
 
 const handleInputChange = () => {
